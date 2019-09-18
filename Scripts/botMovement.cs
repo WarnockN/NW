cca472f2ed37c0884c1ec57b6e0d2708c2dcd75a
WarnockN <54872390+WarnockN@users.noreply.github.com>
2019-09-18 10:42:45 -0400

    Add files via upload

---
+++ Scripts/botMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botMovement : MonoBehaviour
+{
+
+    float min = 2f;
+    float max = 3f;
+    public float distance = 20;
+    // Use this for initialization
+    void Start()
+    {
+
+        min = transform.position.x;
+        max = transform.position.x + distance;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        transform.position = new Vector3(Mathf.PingPong(Time.time * 2, max - min) + min, transform.position.y, transform.position.z);
+
+    }
+}
