1320cda135ce2cc9a438768fa91402afb7f6e65c
WarnockN <54872390+WarnockN@users.noreply.github.com>
2019-09-18 10:44:57 -0400

    Add files via upload

---
+++ Scripts/DoorTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] private Animator myAnimationController;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            myAnimationController.SetBool("DoorOpen", true);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            myAnimationController.SetBool("DoorOpen", false);
+        }
+
+    }
+}
---
+++ Scripts/LevelChanger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+    private int LevelToLoad;
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(1)) {
+            FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1); 
+        }  
+    }
+
+    public void FadeToLevel(int index) {
+        LevelToLoad = index;    
+        animator.SetTrigger("FadeOut");
+
+    }
+
+    public void OnFadeComplete() {
+        SceneManager.LoadScene(LevelToLoad);
+    }
+}
